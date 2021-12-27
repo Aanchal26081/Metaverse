c12b88296c1f272a0cc8962db39ebfc6690ce689
Aanchal <aanchalgaurh2608@gmail.com>
2021-12-27 15:01:31 +0530

    login flow completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const appID = process.env.NEXT_PUBLIC_APP_ID;
+  const serverurl = process.env.NEXT_PUBLIC_SERVER_URL;
+  return (
+    <MoralisProvider 
+    appId={appID} 
+    serverUrl={serverurl}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
+ 
 }
 
 export default MyApp
